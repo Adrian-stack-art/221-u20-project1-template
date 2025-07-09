4ae7b826a0967036d1eaacf70d86061ac6683235
Adrian Acosta Giron <aa3935@students.necsd.net>
2025-07-09 15:29:10 -0400

    project almost done

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,11 +1,14 @@
 
-
+window.addEventListener("load", () => {
+  window.addEventListener('keypress', checkKeyPress)
+})
 
 /*
  * Check ascii code for spacebar and call goToLocation in global to change URL
  */
-function checkKeyPress(e) {
-  if (e.keyCode == 32) {
-    goToLocation('/feed');
+function checkKeyPress(event) {
+  if (event.keyCode == 32) 
+  console.log(event.keycode);{
+    goToLocation('/feed');c
   }
 }
