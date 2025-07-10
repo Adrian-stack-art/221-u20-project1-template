e3ae9334f3e088580d6b318342f2b8dc04fbaa7b
Adrian Acosta Giron <aa3935@students.necsd.net>
2025-07-10 15:39:31 -0400

    project

--- client/public/js/feed.js
+++ client/public/js/feed.js
@@ -1,6 +1,25 @@
-let me = {firstName: 'Brian', lastName: 'Gormanly', age:16};
+function goToLocation(path){
+    window.locationref = path;
+}
 
-Window.addEventListner('lodad', ()=> {
-   let content = document.getElementById('content').innerHTML = "content"
-   
-})
+function checkKeyPress(e) {
+    if (e.keyCode == 32) {
+        e.preventDefault ();
+        goToLocation('http://my.marist.edu');
+    }
+}
+    window.onload = function () {
+        document.body.setAttribute("tabindex", "0");
+        document.body.focus();
+        document.body.addEventListener("keydown", checkKeyPress);
+        const clickTarget = document.getElementById("portal_button");
+        if(clickTarget) {
+        clickTarget.style.cursor = "pointer";
+        clickTarget.addEventListener("click", () => {
+            goToLocation("http://my.marist.edu");
+        });
+    }
+    };
+
+
+
